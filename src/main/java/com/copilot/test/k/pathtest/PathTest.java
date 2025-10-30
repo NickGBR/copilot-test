a597549191e368b9ff65e47b12e1a83514e1a65e
nick <nick@mail.com>
2025-10-30 14:26:04 +0100

    copilot test

--- src/main/java/com/copilot/test/k/pathtest/PathTest.java
+++ src/main/java/com/copilot/test/k/pathtest/PathTest.java
@@ -1,7 +1,12 @@
 package com.copilot.test.k.pathtest;
 
+import com.copilot.test.k.CopilotIgnore;
+
 public class PathTest {
     private final String helloVariableForTest = "hello";
     private final String a = "a";
     private final Float b = new Float(44f);
+    private final Float bvdv = new Float(44f);
+    @CopilotIgnore
+    private final Float badfadsf= new Float(44f);
 }
